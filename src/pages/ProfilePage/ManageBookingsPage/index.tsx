bcc1cf594276c8c97e2edf3f2f87e741becf2064
ygohappy123 <hagiahuy21052003@gmail.com>
2025-01-10 12:12:39 +0700

    add my bookings management and date picker place holder

--- src/pages/ProfilePage/ManageBookingsPage/index.tsx
+++ src/pages/ProfilePage/ManageBookingsPage/index.tsx
@@ -1,5 +1,10 @@
 import { useEffect } from 'react'
+import { useQuery } from 'react-query'
 import useTitle from '@/hooks/useTitle'
+import useAxiosIns from '@/hooks/useAxiosIns'
+import { useNavigate } from 'react-router-dom'
+import Button from '@/components/common/Button'
+import MyBookingCard from '@/pages/ProfilePage/ManageBookingsPage/MyBookingCard'
 
 const ManageBookingsPage = () => {
     useTitle('Stay Mate Hotel | Đơn Đặt Phòng')
@@ -7,9 +12,53 @@ const ManageBookingsPage = () => {
         window.scrollTo(0, 0)
     }, [])
 
+    const navigate = useNavigate()
+    const axios = useAxiosIns()
+
+    const fetchMyBookingsQuery = useQuery(['my-bookings'], {
+        queryFn: () => {
+            return axios.get<IResponseData<IBooking[]>>('/bookings/my-bookings')
+        },
+        refetchOnWindowFocus: false,
+        enabled: true,
+        select: res => res.data
+    })
+
+    const myBookings = fetchMyBookingsQuery.data?.data ?? []
+
+    console.log(myBookings)
+
     return (
-        <div className="flex-1">
-            <h3 className="font-oswald mb-6 text-4xl uppercase leading-[50px] text-primary">Đơn đặt phòng của tôi</h3>
+        <div className="flex w-full flex-col gap-6 lg:w-auto lg:flex-1">
+            <h3 className="font-oswald text-4xl uppercase leading-[50px] text-primary">Đơn đặt phòng của tôi</h3>
+
+            {myBookings.length > 0 ? (
+                <>
+                    {myBookings.map(booking => (
+                        <MyBookingCard key={booking.id} booking={booking} />
+                    ))}
+                </>
+            ) : (
+                <>
+                    <div className="text-left font-semibold text-[#6E6E6E]">
+                        <p className="text-lg">Bạn chưa có đơn đặt phòng nào!</p>
+                        <p>Chúng tôi mong sẽ sớm được gặp bạn tại Stay Mate Hotel &hearts;</p>
+                    </div>
+                    <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-6">
+                        <Button
+                            text="Tìm hiểu thêm"
+                            onClick={() => navigate('/about-us')}
+                            variant="gradient"
+                            className="w-full rounded-full text-lg font-semibold capitalize"
+                        />
+                        <Button
+                            text="Đặt phòng ngay"
+                            onClick={() => navigate('/booking')}
+                            className="w-full rounded-full text-lg font-semibold capitalize"
+                        />
+                    </div>
+                </>
+            )}
         </div>
     )
 }
---
+++ src/services/serviceReservationService.ts
@@ -0,0 +1,169 @@
+import { useEffect, useState } from 'react'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { toast } from 'react-toastify'
+import { onError } from '@/utils/errorsHandler'
+import { getMappedMessage } from '@/utils/resMessageMapping'
+import dayjs from 'dayjs'
+import useAxiosIns from '@/hooks/useAxiosIns'
+import toastConfig from '@/configs/toast'
+
+const serviceReservationService = ({ enableFetching }: { enableFetching: boolean }) => {
+    const axios = useAxiosIns()
+    const queryClient = useQueryClient()
+    const [bookingServices, setBookingServices] = useState<IBookingService[]>([])
+    const [bookingServicesCount, setBookingServicesCount] = useState<Record<BookingServiceStatus, number>>()
+    const [total, setTotal] = useState<number>(0)
+    const [isSearching, setIsSearching] = useState(false)
+
+    const [page, setPage] = useState<number>(1)
+    const [limit] = useState<number>(6)
+    const [query, setQuery] = useState<string>('')
+    const [sort, setSort] = useState<string>('')
+
+    const searchBookingServicesQuery = useQuery(['search-booking-services', query, sort], {
+        queryFn: () => {
+            return axios.get<IResponseData<IBookingService[]>>(
+                `/bookings/booking-services?skip=${limit * (page - 1)}&limit=${limit}&filter=${query}&sort=${sort}`
+            )
+        },
+        keepPreviousData: true,
+        enabled: false,
+        onError: onError,
+        onSuccess: res => {
+            if (!res) return
+            setBookingServices(res.data.data)
+            setTotal(res.data.total as number)
+        }
+    })
+
+    const getAllBookingServicesQuery = useQuery(['booking-services', page, limit], {
+        queryFn: () => {
+            if (!isSearching) {
+                return axios.get<IResponseData<IBookingService[]>>(`/bookings/booking-services?skip=${limit * (page - 1)}&limit=${limit}`)
+            }
+        },
+        keepPreviousData: true,
+        enabled: enableFetching,
+        refetchOnWindowFocus: false,
+        refetchIntervalInBackground: true,
+        refetchInterval: 10000,
+        onError: onError,
+        onSuccess: res => {
+            if (!res) return
+            setBookingServices(res.data.data)
+            setTotal(res.data.total as number)
+        }
+    })
+
+    const getCsvBookingServicesQuery = useQuery(['search-csv-booking-services', query, sort], {
+        queryFn: () => {
+            return axios.get<IResponseData<IBookingService[]>>(`/bookings/booking-services?filter=${query}&sort=${sort}`)
+        },
+        keepPreviousData: true,
+        enabled: false,
+        onError: onError
+    })
+
+    useQuery(['count-booking-services-by-status'], {
+        queryFn: () => {
+            return axios.get<IResponseData<Record<BookingServiceStatus, number>>>('/bookings/booking-services/count-by-status')
+        },
+        keepPreviousData: true,
+        enabled: enableFetching,
+        refetchOnWindowFocus: false,
+        refetchIntervalInBackground: true,
+        refetchInterval: 10000,
+        onError: onError,
+        onSuccess: res => {
+            if (!res) return
+            setBookingServicesCount(res.data.data)
+        }
+    })
+
+    const onFilterSearch = () => {
+        setPage(1)
+        setIsSearching(true)
+        setTimeout(() => searchBookingServicesQuery.refetch(), 300)
+    }
+
+    const onResetFilterSearch = () => {
+        setPage(1)
+        setIsSearching(false)
+        setQuery('')
+        setSort('')
+        setTimeout(() => getAllBookingServicesQuery.refetch(), 300)
+    }
+
+    useEffect(() => {
+        if (isSearching) {
+            searchBookingServicesQuery.refetch()
+        }
+    }, [page])
+
+    const acceptBookingMutation = useMutation({
+        mutationFn: (bookingServiceId: number) => {
+            return axios.post<IResponseData<any>>(`/bookings/accept-booking-services/${bookingServiceId}`)
+        },
+        onError: onError,
+        onSuccess: res => {
+            if (isSearching) {
+                queryClient.invalidateQueries('search-booking-services')
+                searchBookingServicesQuery.refetch()
+            } else {
+                queryClient.invalidateQueries('booking-services')
+            }
+            toast(getMappedMessage(res.data.message), toastConfig('success'))
+        }
+    })
+
+    const rejectBookingMutation = useMutation({
+        mutationFn: (bookingServiceId: number) => {
+            return axios.post<IResponseData<any>>(`/bookings/reject-booking-services/${bookingServiceId}`)
+        },
+        onError: onError,
+        onSuccess: res => {
+            if (isSearching) {
+                queryClient.invalidateQueries('search-booking-services')
+                searchBookingServicesQuery.refetch()
+            } else {
+                queryClient.invalidateQueries('booking-services')
+            }
+            toast(getMappedMessage(res.data.message), toastConfig('success'))
+        }
+    })
+
+    const handOverBookingMutation = useMutation({
+        mutationFn: (bookingServiceId: number) => {
+            return axios.post<IResponseData<any>>(`/bookings/hand-over-booking-services/${bookingServiceId}`)
+        },
+        onError: onError,
+        onSuccess: res => {
+            if (isSearching) {
+                queryClient.invalidateQueries('search-booking-services')
+                searchBookingServicesQuery.refetch()
+            } else {
+                queryClient.invalidateQueries('booking-services')
+            }
+            toast(getMappedMessage(res.data.message), toastConfig('success'))
+        }
+    })
+
+    return {
+        bookingServices,
+        bookingServicesCount,
+        total,
+        page,
+        limit,
+
+        setPage,
+        onFilterSearch,
+        onResetFilterSearch,
+
+        getCsvBookingServicesQuery,
+        acceptBookingMutation,
+        rejectBookingMutation,
+        handOverBookingMutation
+    }
+}
+
+export default serviceReservationService
