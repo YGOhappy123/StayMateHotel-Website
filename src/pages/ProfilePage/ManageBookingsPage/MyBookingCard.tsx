2a784b8539b34c49b876a86c462ef7cfcdc0d138
ygohappy123 <hagiahuy21052003@gmail.com>
2025-01-10 16:05:23 +0700

    my booking management page and room detail page

--- src/pages/ProfilePage/ManageBookingsPage/MyBookingCard.tsx
+++ src/pages/ProfilePage/ManageBookingsPage/MyBookingCard.tsx
@@ -1,13 +1,21 @@
 import { useMemo, useState } from 'react'
+import { UseQueryResult } from 'react-query'
 import { twMerge } from 'tailwind-merge'
 import { differenceInDays, format } from 'date-fns'
 import { vi } from 'date-fns/locale'
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/Accordion'
-import { getMappedBookingStatus } from '@/utils/bookingStatusMapping'
+import { Dialog, DialogTrigger } from '@/components/ui/Dialog'
+import { getMappedBookingServiceStatus, getMappedBookingStatus, getMappedPaymentMethod } from '@/utils/bookingStatusMapping'
 import dayjs from 'dayjs'
+import bookingService from '@/services/bookingService'
+import serviceReservationService from '@/services/serviceReservationService'
+import ConfirmationDialog from '@/components/common/ConfirmationDialog'
+import BookServiceDialog from '@/pages/ProfilePage/ManageBookingsPage/BookServiceDialog'
 
 type MyBookingCardProps = {
     booking: IBooking
+    services: IService[]
+    fetchMyBookingsQuery: UseQueryResult<IResponseData<IBooking[]>, unknown>
 }
 
 const baseButtonClass =
@@ -21,7 +29,11 @@ const statusTheme = {
     PaymentDone: 'bg-yellow-100 border-yellow-600 text-yellow-600'
 }
 
-const MyBookingCard = ({ booking }: MyBookingCardProps) => {
+const MyBookingCard = ({ booking, services, fetchMyBookingsQuery }: MyBookingCardProps) => {
+    const { cancelBookingMutation } = bookingService({ enableFetching: false })
+    const { bookServiceMutation } = serviceReservationService({ enableFetching: false })
+    const [bookServiceDialogOpen, setBookServiceDialogOpen] = useState(false)
+
     const bookingDays = Math.abs(differenceInDays(new Date(booking.checkInTime), new Date(booking.checkOutTime)))
     const totalPaymentAmount = useMemo(() => {
         return booking.payments.reduce((total, payment) => total + (payment.amount ?? 0), 0)
@@ -87,8 +99,9 @@ const MyBookingCard = ({ booking }: MyBookingCardProps) => {
                                     </p>
                                     <p className="flex items-center justify-between text-lg">
                                         <span className="font-semibold">Đơn giá theo ngày: </span>
-                                        {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(room!.unitPrice as number)}
+                                        {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(room.unitPrice!)}
                                     </p>
+                                    {index !== booking.bookingRooms.length - 1 && <div className="mt-1 h-0.5 bg-black/10"></div>}
                                 </div>
                             ))}
                         </AccordionContent>
@@ -99,22 +112,55 @@ const MyBookingCard = ({ booking }: MyBookingCardProps) => {
                 <div className="flex flex-col gap-3">
                     <AccordionItem value="item-2" className="border-none">
                         <AccordionTrigger>
+                            <h3 className="text-2xl font-semibold text-primary">Các dịch vụ được đặt kèm</h3>
+                        </AccordionTrigger>
+                        <AccordionContent className="p-0">
+                            {booking.bookingServices.map((service, index) => (
+                                <div key={index} className="mt-3 flex flex-col gap-2">
+                                    <h4 className="text-xl font-semibold text-secondary">Mã đơn {service.id!.toString().padStart(2, '0')}:</h4>
+                                    <p className="flex items-center justify-between text-lg">
+                                        <span className="font-semibold">Tên dịch vụ: </span>
+                                        {service.name}
+                                    </p>
+                                    <p className="flex items-center justify-between text-lg">
+                                        <span className="font-semibold">Đơn giá và số lượng: </span>
+                                        {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(service.unitPrice!)} -{' '}
+                                        {service.quantity?.toString().padStart(2, '0')}
+                                    </p>
+                                    <p className="flex items-center justify-between text-lg">
+                                        <span className="font-semibold">Trạng thái: </span>
+                                        {getMappedBookingServiceStatus(service.status!)}
+                                    </p>
+                                    {index !== booking.bookingServices.length - 1 && <div className="mt-1 h-0.5 bg-black/10"></div>}
+                                </div>
+                            ))}
+                        </AccordionContent>
+                    </AccordionItem>
+                </div>
+
+                <div className="h-1 bg-black/10"></div>
+                <div className="flex flex-col gap-3">
+                    <AccordionItem value="item-3" className="border-none">
+                        <AccordionTrigger>
                             <h3 className="text-2xl font-semibold text-primary">Lịch sử thanh toán</h3>
                         </AccordionTrigger>
                         <AccordionContent className="p-0">
                             {booking.payments.map((payment, index) => (
                                 <div key={index} className="mt-3 flex flex-col gap-2">
-                                    <h4 className="text-xl font-semibold text-secondary">
-                                        Mã giao dịch {(payment.id ?? 0).toString().padStart(2, '0')}:
-                                    </h4>
-                                    {/* <p className="flex items-center justify-between text-lg">
-                                        <span className="font-semibold">Thông tin phòng: </span>
-                                        Mã {room!.roomNumber} - Tầng {room!.floor} - Loại {room!.roomClass}
+                                    <h4 className="text-xl font-semibold text-secondary">Mã giao dịch {payment.id!.toString().padStart(2, '0')}:</h4>
+                                    <p className="flex items-center justify-between text-lg">
+                                        <span className="font-semibold">Số tiền giao dịch: </span>
+                                        {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(payment.amount!)}
                                     </p>
                                     <p className="flex items-center justify-between text-lg">
-                                        <span className="font-semibold">Đơn giá theo ngày: </span>
-                                        {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(room!.unitPrice as number)}
-                                    </p> */}
+                                        <span className="font-semibold">Hình thức giao dịch: </span>
+                                        {getMappedPaymentMethod(payment.method!)}
+                                    </p>
+                                    <p className="flex items-center justify-between text-lg">
+                                        <span className="font-semibold">Thời gian giao dịch: </span>
+                                        {dayjs(payment.paymentTime).format('DD/MM/YYYY HH:mm:ss')}
+                                    </p>
+                                    {index !== booking.payments.length - 1 && <div className="mt-1 h-0.5 bg-black/10"></div>}
                                 </div>
                             ))}
                         </AccordionContent>
@@ -136,6 +182,51 @@ const MyBookingCard = ({ booking }: MyBookingCardProps) => {
                         <span className="font-semibold">Số tiền cần thu thêm: </span>
                         {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(booking.totalAmount - totalPaymentAmount)}
                     </p>
+                </div>
+
+                <div className="h-1 bg-black/10"></div>
+                <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-6">
+                    <Dialog open={bookServiceDialogOpen} onOpenChange={setBookServiceDialogOpen}>
+                        <DialogTrigger asChild>
+                            <button
+                                disabled={booking.status !== 'CheckedIn'}
+                                className={twMerge(
+                                    `${baseButtonClass} border-yellow-600 bg-yellow-100 text-yellow-600 disabled:border-gray-600 disabled:bg-gray-100 disabled:text-gray-600`
+                                )}
+                            >
+                                Đặt dịch vụ
+                            </button>
+                        </DialogTrigger>
+                        <BookServiceDialog
+                            isOpen={bookServiceDialogOpen}
+                            closeDialog={() => setBookServiceDialogOpen(false)}
+                            services={services}
+                            bookingId={booking.id}
+                            bookServiceMutation={bookServiceMutation}
+                            fetchMyBookingsQuery={fetchMyBookingsQuery}
+                        />
+                    </Dialog>
+
+                    <ConfirmationDialog
+                        Trigger={
+                            <button
+                                disabled={
+                                    booking.status === 'Cancelled' || // Already cancelled
+                                    (booking.status !== 'Pending' && booking.status !== 'Confirmed') // Not pending or confirmed
+                                }
+                                className={twMerge(
+                                    `${baseButtonClass} border-red-600 bg-red-100 text-red-600 disabled:border-gray-600 disabled:bg-gray-100 disabled:text-gray-600`
+                                )}
+                            >
+                                Hủy đơn
+                            </button>
+                        }
+                        title="Xác nhận hủy đơn"
+                        body="Bạn có chắc muốn hủy đơn này không? Bạn sẽ không nhận lại được tiền cọc (nếu có). Thao tác này sẽ không thể hoàn tác."
+                        onConfirm={async () => {
+                            await cancelBookingMutation.mutateAsync(booking.id).then(() => fetchMyBookingsQuery.refetch())
+                        }}
+                    />
                 </div>
             </div>
         </Accordion>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,49 +3,49 @@ export default {
     darkMode: ['class'],
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-    	extend: {
-    		colors: {
-    			primary: '#588157',
-    			secondary: '#A3B18A',
-    			accent: '#344E41',
-    			tertiary: '#3A5A40',
-    			ivory: '#F3ECDC'
-    		},
-    		maxWidth: {
-    			container: '1400px'
-    		},
-    		height: {
-    			topbar: '60px',
-    			navbar: '90px'
-    		},
-    		backgroundImage: {
-    			poster: 'linear-gradient(to right, rgb(52 78 65 / 0.95) 32%, rgb(0 0 0 / 0.13) 100%)',
-    			'image-cover': 'linear-gradient(to bottom, rgb(0 0 0 / 0.04) 0%, rgb(52 78 65 / 0.83) 66%)',
-    			'banner-cover': 'linear-gradient(to right, rgb(52 78 65 / 1) 0%, rgb(0 0 0 / 0.11) 100%)'
-    		},
-    		keyframes: {
-    			'accordion-down': {
-    				from: {
-    					height: '0'
-    				},
-    				to: {
-    					height: 'var(--radix-accordion-content-height)'
-    				}
-    			},
-    			'accordion-up': {
-    				from: {
-    					height: 'var(--radix-accordion-content-height)'
-    				},
-    				to: {
-    					height: '0'
-    				}
-    			}
-    		},
-    		animation: {
-    			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out'
-    		}
-    	}
+        extend: {
+            colors: {
+                primary: '#588157',
+                secondary: '#A3B18A',
+                accent: '#344E41',
+                tertiary: '#3A5A40',
+                ivory: '#F3ECDC'
+            },
+            maxWidth: {
+                container: '1400px'
+            },
+            height: {
+                topbar: '60px',
+                navbar: '90px'
+            },
+            backgroundImage: {
+                poster: 'linear-gradient(to right, rgb(52 78 65 / 0.95) 32%, rgb(0 0 0 / 0.13) 100%)',
+                'image-cover': 'linear-gradient(to bottom, rgb(0 0 0 / 0.04) 0%, rgb(52 78 65 / 0.83) 66%)',
+                'banner-cover': 'linear-gradient(to right, rgb(52 78 65 / 1) 0%, rgb(0 0 0 / 0.11) 100%)'
+            },
+            keyframes: {
+                'accordion-down': {
+                    from: {
+                        height: '0'
+                    },
+                    to: {
+                        height: 'var(--radix-accordion-content-height)'
+                    }
+                },
+                'accordion-up': {
+                    from: {
+                        height: 'var(--radix-accordion-content-height)'
+                    },
+                    to: {
+                        height: '0'
+                    }
+                }
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out'
+            }
+        }
     },
     plugins: [import('tailwindcss-animate')]
 }
